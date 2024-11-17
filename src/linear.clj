fc0fef605ef7871af215475c1560f8024b685463
Leonchik09 <vasya.leonov2009@gmail.com>
2024-11-17 16:44:49 +0300

    Solution easy version corrected x2

--- src/linear.clj
+++ src/linear.clj
@@ -1,36 +1,28 @@
 (ns linear)
 
-(defn operation
-  [f & v]
+(defn operation [f & v]
   (apply mapv f v))
 (def v+ (partial operation +))
 (def v- (partial operation -))
 (def v* (partial operation *))
 (def vd (partial operation /))
-(defn dot
-  [& v]
+(defn dot [& v]
   (if (empty? v)
     0
-    (apply + (apply v* v))
-    ))
-(defn v*s
-  [v & s]
-  (mapv #(* % (apply * s)) v))
+    (apply + (apply v* v))))
+(defn v*s [v & s]
+  (mapv #(apply * % s) v))
 (def m+ (partial operation v+))
 (def m- (partial operation v-))
 (def m* (partial operation v*))
 (def md (partial operation vd))
-(defn m*s
-  [m & s]
+(defn m*s [m & s]
   (mapv #(v*s % (apply * s)) m))
-(defn transpose
-  [m]
+(defn transpose [m]
   (apply mapv vector m))
-(defn m*v
-  [m v]
+(defn m*v [m v]
   (mapv #(dot % v) m))
-(defn m*m
-  [& m]
+(defn m*m [& m]
   (reduce
     (fn [m1 m2]
       (operation #(m*v (transpose m2) %) m1))
